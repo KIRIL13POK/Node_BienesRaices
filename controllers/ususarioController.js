781f96eb3fc7788a88820e0399a28ea93d57cf13
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-07-06 04:14:23 +0200

    Añadiendo Más rutas

--- controllers/ususarioController.js
+++ controllers/ususarioController.js
@@ -2,9 +2,13 @@
 const formularioLogin =( (req, res) => {
     res.render('auth/login')
 });
+const formularioRegistro =( (req, res) => {
+    res.render('auth/registro')
+});
 
 module.exports = {
     formularioLogin,
+    formularioRegistro
 }
 
 
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { formularioLogin } = require('../controllers/ususarioController.js')
+const {
+       formularioLogin,
+       formularioRegistro, } = require('../controllers/ususarioController.js')
 
 const router = express.Router();
 
 router.get('/login', formularioLogin)
+router.get('/registro', formularioRegistro)
 
 
 module.exports = router
